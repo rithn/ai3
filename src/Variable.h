11c41d0455bf9da36f9f51487b3f3a7736975537
sushant21 <rathisushant5@gmail.com>
2018-09-24 15:19:16 +0530

    Minor bug fix

--- src/Variable.h
+++ src/Variable.h
@@ -9,17 +9,22 @@ using namespace std;
 struct Variable{
     public:
     int id;
+    bool isTrue;
     static int counter;
     static map<string,int> variableToId;
     static map<int,string> idToVariable;
     // Variable(){}   
-    Variable(string s){
+    Variable(string s, bool t){
+        isTrue=t;
         if(variableToId.find(s)==variableToId.end()){
             counter++;
             variableToId[s]=counter;
             idToVariable[counter]=s;
+            id=counter;
         }
-        id=counter;
+        else{
+            id=variableToId[s];
+        }
     }
     virtual string repr(){}
 
@@ -31,8 +36,7 @@ struct Variable{
 * A(node,agency)  */
 struct NodeAgency: Variable{    
     int node,agency;
-    bool isTrue;
-    NodeAgency(int n, int a, bool t): node(n), agency(a), isTrue(t), Variable(repr(n,a)){}
+    NodeAgency(int n, int a, bool t): node(n), agency(a), Variable(repr(n,a), t){}
     string repr(int n, int a){
         return "-"+to_string(n)+","+to_string(a);
     }
@@ -42,8 +46,7 @@ struct NodeAgency: Variable{
 *  assume node1>node2 */
 struct NodePairAgency: Variable{    
     int node1,node2,agency;
-    bool isTrue;
-    NodePairAgency(int n1, int n2, int a, bool t): node1(n1), node2(n2), agency(a), isTrue(t), Variable(repr(n1,n2,a)){}
+    NodePairAgency(int n1, int n2, int a, bool t): node1(n1), node2(n2), agency(a), Variable(repr(n1,n2,a), t){}
     string repr(int n1, int n2, int a){
         return "#"+to_string(n1)+","+to_string(n2)+","+ to_string(a);
     }
@@ -53,8 +56,7 @@ struct NodePairAgency: Variable{
 /* A(node,agency1)^ !A(node,agency2) */
 struct AgencyPairNode: Variable{    
     int agency1,agency2,node;
-    bool isTrue;
-    AgencyPairNode(int a1, int a2, int n, bool t): agency1(a1), agency2(a2), node(n), isTrue(t), Variable(repr(a1,a2,n)){}
+    AgencyPairNode(int a1, int a2, int n, bool t): agency1(a1), agency2(a2), node(n), Variable(repr(a1,a2,n),t){}
     string repr(int a1,int a2, int n){
         return "$"+to_string(a1)+","+to_string(a2)+","+ to_string(n);
     }
